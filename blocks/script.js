bdc4571a2d8d06d7d0b925fe617348614f2a66b8
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-05-18 15:28:54 +0300

    Feat popup popup буду менятьjs

--- blocks/script.js
+++ blocks/script.js
@@ -1,24 +1,29 @@
 let container = document.querySelector('.container');
 
-let profile__editbutton = document.querySelector('.profile__edit-button');
-profile__editbutton.addEventListener('click', openPopup);
+let editButton = container.querySelector('.profile__edit-button');
+editButton.addEventListener('click', openPopup);//ввод текста
 
 let popupClose = document.querySelector('.popup__close');
 popupClose.addEventListener('click', closePopup);
 
-let popupSave = document.querySelector('.popup__input-save');
-let popupName = document.querySelector('.popup__field_name');
-let popupJob = document.querySelector('.popup__field_job');
+let popupSave = container.querySelector('.popup__save');
+let popupName = container.querySelector('.popup__profile__title');
+let popupJob = container.querySelector('.popup__profile__subtitle');
 let fullname = document.querySelector('.profile__full-name');
 let jobs = document.querySelector('.profile__jobs');
 let popup = document.querySelector('.popup');
 let formSubmit = document.querySelector('.popup__form');
 let profile__addbutton = document.querySelector('.profile__add-button');
 
-let heart = document.querySelector('selector');
-heart.addEventListener( 'click', () =>
-heart.classList.toggle('heart-active') )
+let heart = document.querySelector('elements__heart');
 
+heart.addEventListener( 'click', () =>
+heart.classList.toggle('heart-active') )//Переключим классы
+
+//Переключим классы
+//elem.classList.toggle("you");
+//elem.classList.toggle("he");
+//console.log(elem.classList);
 
 function openPopup() { //открытие popup
     popup.classList.add('popup_opened');
