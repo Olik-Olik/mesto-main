fc34111d76dc983e1da5711ea813e72ca95b1b67
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-05-20 16:40:29 +0300

    Feat popup close is working Лед тронулся

--- blocks/script.js
+++ blocks/script.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container');
 const heartButton = document.querySelector('.elements__heart');
 heartButton.addEventListener( 'click', () =>
 heartButton.classList.toggle('heart-active'));//переключит на черное хотелось бы
-let popupClose = container.querySelector('.popup__close');
+/*let popupClose = container.querySelector('.popup__close');*/
 let popupSave = container.querySelector('.popup__save');
 let popupName = container.querySelector('.popup__field_name');
 let popupJob = container.querySelector('.popup__field_job');
@@ -19,17 +19,17 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = container.querySelector('.popup');
 editButton.addEventListener('click',function (){
 popup.classList.add('popup_visible');})
-
-
     /*classList.add('popup_opened');*/
 /*function closePopup() {
      popup.classList.remove('popup_opened');
 /*function closePopup() { //закрытие popup
     popup.classList.remove ='popup_visible';
 }*/
+let popupClose = container.querySelector('.popup__close');
+popupClose.addEventListener('click',function (){
+popup.classList.remove ('popup_visible');})
 
-
-editButton.addEventListener('click', openPopup);
+/*editButton.addEventListener('click', openPopup);*/
 /*popupClose.addEventListener('click', closePopup);}*/
 
 
