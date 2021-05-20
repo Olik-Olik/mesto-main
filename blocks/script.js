076d30b1855e81426508163ffa9b00196438855d
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-05-20 16:20:08 +0300

    Feat  попап открытие

--- blocks/script.js
+++ blocks/script.js
@@ -1,24 +1,22 @@
-let container = document.querySelector('.container');
+const container = document.querySelector('.container');
 console.log(container.classList);
-let heartButton = document.querySelector('.elements__heart');
+const heartButton = document.querySelector('.elements__heart');
 heartButton.addEventListener( 'click', () =>
 heartButton.classList.toggle('heart-active'));//переключит на черное хотелось бы
-
-let editButton = container.querySelector('.profile').querySelector('.profile__info').querySelector('.profile__edit-button');
 let popupClose = container.querySelector('.popup__close');
-
 let popupSave = container.querySelector('.popup__save');
-
 let popupName = container.querySelector('.popup__field_name');
 let popupJob = container.querySelector('.popup__field_job');
-
 let fullname = document.querySelector('.profile__full-name');
 let jobs = document.querySelector('.profile__jobs');
 let popup = container.querySelector('.popup');
 let formSubmit = container.querySelector('.popup__form');
-
 let addButton = container.querySelector('.profile__add-button');
 
+/*
+const editButton = container.querySelector('.profile').querySelector('.profile__info').querySelector('.profile__edit-button');
+*/
+const editButton = document.querySelector('.profile__edit-button');
 function openPopup() {
     //открытие popup
     popup.classList.add('popup_visible');
